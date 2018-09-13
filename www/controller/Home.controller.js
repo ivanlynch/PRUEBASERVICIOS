66453913f193ec73226e9d3ebb6d36d82b1358c5
Ivan Ignacio Vera Lynch <ivan.vera@neoris.cxnetworks.net>
2018-09-13 18:00:10 -0300

    Ultimos cambios al 13/9

--- www/controller/Home.controller.js
+++ www/controller/Home.controller.js
@@ -3,24 +3,13 @@ sap.ui.define([
 	"pae/logistica/controller/BaseController",
 	'sap/ui/model/json/JSONModel',
 	"sap/m/MessageToast"
-], function (jQuery, BaseController, MessageToast, JSONModel) {
+], function (jQuery, BaseController, MessageToast, JSONModel, ODataModel) {
 	"use strict";
 
 	return BaseController.extend("pae.logistica.controller.Home", {
 		onInit: function(){
-			var oModel = new sap.ui.model.odata.v4.ODataModel({
-				groupId : "$direct",
-				synchronizationMode : "None",
-				serviceUrl : "https://sandbox.api.sap.com/sapb1/b1s/v2/"
-			},
-			true,
-			"S0016198377",
-			"@Lynch12");
-			var mHeaders = {
-				"APIKey" : "s9mEiQAd34aGhHLHC3XTq6y2UiYyhap7"
-			}
-			oModel.setHeaders(mHeaders);
-			this.getView().setModel(oModel);
+			var oModel = this.getView().getBindingContext("Orders");
+			console.log(oModel);
 		},
 		onPressAvisos: function(event){
 			var oComponent = this.getOwnerComponent();
